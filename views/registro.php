f7bf8f5ee33b9eab3d9923acc984c53546d640a2
JhonsHZ <jh5587309@gmail.com>
2021-09-29 03:24:53 -0600

    servicio de smtp
    ya esta el registro y ya da el correo

--- views/registro.php
+++ views/registro.php
@@ -1,7 +1,6 @@
 
 
-
-<?php include("Model/db.php") ?>
+<?php include("../Model/db.php");  ?>
 
 
 
@@ -15,32 +14,50 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/estilos.css">
     <link rel="stylesheet" href="/node_modules/sweetalert2/dist/sweetalert2.css">
-    <title>LOGIN</title>
+    <title>REGISTRO</title>
 </head>
 
 <body>
     <div class="container">
         <div class="row justify-content-center pt-5 mt-5 m-1">
             <div class="col-md-6 col-sm-8 col-xl-4 col-lg-5 formulario">
-                <form action="views/validar.php" method="post">
+                <form action="validarRegistro.php" method="post">
                     <div class="form-group text-center pt-3">
-                        <h1 class="text-light">INICIAR SESIÓN</h1>
+                        <h1 class="text-light">REGISTRO</h1>
                     </div>
                     <div class="form-group mx-sm-4 pt-3">
-                    <p>Usuario <input type="text" class="form-control" placeholder="Ingrese su Usuario" name="usuario"></p>
+                    <p>Usuario <input type="text" class="form-control" placeholder="Ingrese su Usuario" name="user"></p>
                     </div>
                     <div class="form-group mx-sm-4 pb-3">
-                    <p>Contraseña <input type="text" class="form-control" placeholder="Ingrese su Contraseña" name="contraseña"></p>
+                    <p>Contraseña <input type="text" class="form-control" placeholder="Ingrese su Contraseña" name="passwd"></p>
                     </div>
+
+                   
+
+                    <div class="form-group mx-sm-4 pb-3">
+                    <p>Nombre Perfil <input type="text" class="form-control" placeholder="Ingrese su Nombre de Perfil" name="name"></p>
+                    </div>
+
+                    <div class="form-group mx-sm-4 pb-3">
+                    <p>Apellidos <input type="text" class="form-control" placeholder="Ingrese sus Apellidos" name="lastname"></p>
+                    </div>
+
+                    <div class="form-group mx-sm-4 pb-3">
+                    <p>Telefono <input type="text" class="form-control" placeholder="Ingrese su Telefono" name="telephone"></p>
+                    </div>
+
+                    <div class="form-group mx-sm-4 pb-3">
+                    <p>Correo <input type="text" class="form-control" placeholder="Ingrese su Correo" name="mail"></p>
+                    </div>
+
+                  
+
                     <div class="form-group mx-sm-4 pb-2">
-                        <input type="submit" class="btn btn-block ingresar" value="INGRESAR">
+                        <input type="submit" name="save" class="btn btn-block ingresar" value="REGISTRAR">
                     </div>
-                    <div class="form-group mx-sm-4 text-right">
-                        <span class=""><a href="#" class="olvide">Olvide mi contraseña?</a></span>
+                  
                     </div>
-                    <div class="form-group text-center">
-                        <span><a href="views/registro.php" class="olvide1">REGISTRARSE</a></span>
-                    </div>
+                   
                 </form>
             </div>
         </div>
---
+++ views/validarRegistro.php
@@ -0,0 +1,62 @@
+<?php
+
+include("../Model/db.php"); 
+
+if(isset($_POST['save'])){
+    $user = $_POST['user'];
+    $password = $_POST['passwd'];
+    $name = $_POST['name'];
+    $lastname = $_POST['lastname'];
+    $telephone = $_POST['telephone'];
+    $email = $_POST['mail'];
+    $direcion = "../../img/";
+    mkdir($direcion . $user);
+   
+    $consult = "INSERT INTO usuario(nombreUsuario, password, NombrePerfil, Apellidos, Telefono, Correo, estado) VALUES ('$user','$password','$name','$lastname','$telephone','$email','2')";
+    $error = mysqli_query($conexion,$consult);
+
+        if(!$error){
+            die("Error");
+        }
+    
+}
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try{
+
+    //Server settings
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'pruebaproy23@gmail.com';                 // SMTP username
+    $mail->Password = 'jh5587309';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    //Recipients
+    $mail->setFrom('pruebaproy23@gmail.com', 'Galeria Lunar');
+    $mail->addAddress($email);     // Add a recipient
+  //  $mail->addReplyTo('jhon56489754@gmail.com');
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Bienvenidos a Galeria Lunar';
+   $mail->Body = "Nombre de usuario es: ". $user . "\nla contra es: " . $password;
+    //$mail->Body = $password;
+	
+
+ //   $mail->Body    = $mailContent ;
+    $mail->send();
+    echo 'El mensaje se envio correctamente';
+}
+catch (Exception $e){
+    echo 'Hubo un error ', $mail->ErrorInfo;
+}
+
+
+header("location:Login.php");
+
+?>
